c354349951d09702e0ba843c5c3965d1f65d6738
tomcattigerkkk <tomcat.tiger@vip.163.com>
2018-03-26 14:58:19 +0800

    update something new for capstone

---
+++ Capstone/auto/proposal.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "proposal"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "ctex")
+   (LaTeX-add-labels
+    "sec:org61c381c"
+    "sec:org3144931"
+    "sec:orge54e3b4"
+    "sec:org768782f"
+    "sec:orgfff231d")
+   (LaTeX-add-bibliographies
+    "../../../../LibData/Bibliography/bib"))
+ :latex)
+
