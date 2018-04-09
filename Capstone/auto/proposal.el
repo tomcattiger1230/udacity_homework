46266e42443a7e51c83606f33f812404f261b29d
tomcattigerkkk <tomcat.tiger@vip.163.com>
2018-04-09 11:02:46 +0800

    edit the proposal file

--- Capstone/auto/proposal.el
+++ Capstone/auto/proposal.el
@@ -5,12 +5,12 @@
                      '(("article" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -29,17 +29,23 @@
     "hyperref"
     "ctex")
    (LaTeX-add-labels
-    "sec:org7dc784c"
-    "sec:org5b1e0f4"
-    "sec:org029b502"
-    "sec:org20914cf"
-    "sec:org228f37f"
-    "sec:org1f15911"
-    "sec:org4b1bf69"
-    "sec:orga031eb5"
-    "sec:org782b769"
+    "sec:org9e456b5"
+    "sec:org6f63352"
+    "sec:org9cf5769"
+    "sec:orgd80bbcc"
+    "sec:orgef49ea3"
+    "sec:org78a5802"
+    "sec:orgc29b8e9"
+    "sec:org454f235"
     "eq:crossE"
-    "eq:lossF")
+    "eq:lossF"
+    "eq:kaggelE"
+    "sec:orgd7a83e8"
+    "sec:org98086fe"
+    "sec:orga35ff75"
+    "sec:orga912f23"
+    "sec:org6d2ebe4"
+    "sec:org4778596")
    (LaTeX-add-bibliographies
     "../../../../LibData/Bibliography/bib"))
  :latex)
